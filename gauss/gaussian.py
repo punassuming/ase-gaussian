ba9766a365ed841643ffcab95f043d6cda874b40
walesi@andrew.cmu.edu <rlab>
2011-07-19 14:04:30 -0400

    Added docstring with working idea to Gaussian.py

--- gauss/gaussian.py
+++ gauss/gaussian.py
@@ -42,32 +42,72 @@ class Gaussian():
         
             basename : string
              basename used for gaussian log and checkpoint files and binary
+             extensions looked for:
+                 gc              : gaussian ASE object as binary file
+                 log | g03 | g09 : gaussian output log file
+                 chk | fchk      : gaussian checkpoint file
 
             output : string
-             different name of output file if the user desires, 
+             different basename of output file to allow copying old job,
              will revert to basename if not chosen
 
             debug : integer
              logging debug level
 
             kwargs : **
-             contains many possible items, listed below
+             includes all major features of gaussian
 
+                atoms : ASE.Atoms object
+                 atoms object is attached to calculator to save information
+                
                 xc : text
                  checked against possible gassian parameters
+                 setup currently only for DFT jobs:
+                     ['B3LYP',
 
                 basis : text
                  checked against possible gaussian parameters
 
-                keywords : dict
-                 contains optional keyword arguments to place in gaussian input
+                job : dict
+                 contains optional keyword arguments to place in gaussian input,
+                 keys are available as the Gaussian job options:
 
-                mem : options to specify 
-        
-        
-        
-        
-        Asign filenames derived from basename
+                    ['Density','Force','Freq','Geom','Guess','Massage','NMR',
+                     'Opt','Polar','Pop','SCRF','SP','Scan','Volume']
+
+                 note: Opt and Freq can be together, but not with SCRF
+
+                 e.g. a standard optimization will be {'opt':[]}
+                 * a more complex string would be:
+                    {'opt':['tight'],'freq':[],'pop':['Hirshfield','NBO']}
+
+                mem : float
+                 value in GB to reserve for memory
+
+                debug : logging level used for parser and job creation
+
+            Instead of an input file, which is typically used for gaussian
+            jobs, as ASE atoms object with calculator information is saved for
+            easier job creation.  This information contains all the job
+            submission information in addition to the atom positions.  In
+            order to obtain the energies, the log and checkpoint files are
+            parsed to determine if the job has already been run and has
+            completed successfully.  If so, the energy is returned, otherwise,
+            the input string is constructed based on the atoms object and the
+            job options specified on init.  This string is passed to gaussian
+            through stdin, preventing the need for temporary input files. 
+
+            Examples
+            
+             requires water file to already exist
+            >>> gau = Gaussian('water')
+
+             restart previous optimization
+            >>> gau = Gaussian('CO2', job = {'opt':['restart']})
+
+        """
+
+        #Asign filenames derived from basename
         self.basename = basename
         self.log = basename + '.log'
         self.chk = basename + '.chk'
